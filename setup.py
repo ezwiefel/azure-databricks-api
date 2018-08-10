434c9d48eec8178e1b951d02381c06a3a8266f49
Erik Zwiefel <erik.zwiefel@microsoft.com>
2018-08-10 13:56:10 -0500

    Updates setup.py to not generate AUTHORS or ChangeLog files

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
+
 from setuptools import setup
+
+# Ask pbr to not generate AUTHORS file if environment variable does not require it
+if not os.environ.get('SKIP_GENERATE_AUTHORS'):
+    os.environ['SKIP_GENERATE_AUTHORS'] = '1'
+
+# Ask pbr to not generate ChangeLog file if environment variable does not require it
+if not os.environ.get('SKIP_WRITE_GIT_CHANGELOG'):
+    os.environ['SKIP_WRITE_GIT_CHANGELOG'] = '1'
 
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
